bc751d663908214acd644cd42be77d60c065734d
Andreas karlsson <andreas.k8505@gmail.com>
2025-12-17 14:08:50 +0100

    Refactor Main class to enable GUI execution without console seeding

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-/*public class Main {
+public class Main {
      static void main() {
         new App().run();
     }
-}*/
+}
 
 // change to this if you want to run GUI without console seeding
 //-----------------------------GUI-----------------------------
-public class Main {
+/*public class Main {
      static void main() {
         App app = new App();
         app.runWithoutConsole(); // new method that only seeds data
         new ShopGUI(app);
     }
-}
+}*/
